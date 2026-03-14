d081bee08f4a5ebbe2da3aba3c5dfd66a1d57ec0
RAJESH2132 <rajeshnagurla@gmail.com>
2026-03-14 22:43:57 +0530

    push final spring data rest

--- src/main/java/com/springboot/app/employee/dao/EmployeeRepository.java
+++ src/main/java/com/springboot/app/employee/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.springboot.app.employee.dao;
 
 import com.springboot.app.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
